d085948687f38c756078433924de6af9e891513a
efraimnabil <efraimefefraim@gmail.com>
2024-10-22 14:25:47 +0300

    Add basic HTTP server with support for GET, POST, PUT, and DELETE requests

--- server.js
+++ server.js
@@ -1,3 +1,30 @@
 const http = require("http");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 5001;
+
+const server = http.createServer((req, res) => {
+    switch (req.method) {
+        case "GET":
+            getReq(req, res);
+            break;
+        case "POST":
+            getPost(req, res);
+            break;
+        case "PUT":
+            getPut(req, res);
+            break;
+        case "DELETE":
+            getDelete(req, res);
+            break;
+        default:
+            res.statusCode = 404;
+            res.setHeader("Content-Type", "application/json");
+            res.write(JSON.stringify({ title: "Not Found", message: "Route Not Found" }));
+            res.end();
+    }
+})
+
+server.listen(PORT, () => {
+    console.log(`server started on port: ${PORT}`);
+})
